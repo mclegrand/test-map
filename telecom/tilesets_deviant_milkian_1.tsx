859b9da0018ddc264d051ae4e4d0de8cec852950
Marc Jeanmougin <marc@jeanmougin.fr>
2021-02-23 16:57:47 +0100

    4th floor map

---
+++ telecom/tilesets_deviant_milkian_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="tilesets_deviant_milkian_1" tilewidth="32" tileheight="32" tilecount="512" columns="32">
+ <image source="tilesets_deviant_milkian_1.png" width="1024" height="512"/>
+</tileset>
